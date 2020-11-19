da15c78cb456936ce4e2a0a3bf404b41f74f3cf1
Leticia P. May <43357500+leticiapmay99@users.noreply.github.com>
2020-11-18 22:07:55 -0300

    cors

--- backend/src/config/custom-express.js
+++ backend/src/config/custom-express.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 const expressValidator = require('express-validator'); 
+app.use(cors())
 app.use(bodyParser.urlencoded({
     extended: true
 }));
