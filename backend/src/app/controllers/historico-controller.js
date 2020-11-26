fc26ec102aa09fcf7275126a9a6257e515ebb55d
Giovani Meneguel <34317514+GioAM@users.noreply.github.com>
2020-11-26 08:55:30 -0300

    ajuste no arduino

--- backend/src/app/controllers/historico-controller.js
+++ backend/src/app/controllers/historico-controller.js
@@ -14,7 +14,43 @@ class HistoricoController {
 
     requisicao() {
         return function(req, resp) {
-            console.log(req.body);
+            cartaoDao.buscaPorRfid(req.body.rfid)
+                .then(cartao => {
+                    if(cartao == null || cartao == ""){
+                        cartaoDao.adiciona("Cartão não registrado", req.body.rfid, null)
+                            .then(cartaoId =>{
+                                historicoDao.gravarHistorico(cartaoId, req.body.dispositivo, 0);
+                                historicoDao.adicionarCartao(cartaoId, req.body.dispositivo, 0);
+                                return resp.json({autorizado:false});
+                            }).catch(erro => {
+                                resp.status(500).end();
+                                console.log(erro);
+                            });;
+                    }else{
+                        historicoDao.verificarAutorizado(req.body.rfid, req.body.dispositivo)
+                            .then(autorizado =>{
+                                var aut_bool = false;
+                                var aut_num = 0;
+                                if(autorizado == null || autorizado == ""){
+                                    aut_bool = false;
+                                    aut_num = 0;
+                                }
+                                else if(autorizado.autorizado){
+                                    aut_bool = true;
+                                    aut_num = 1;
+                                }
+                                historicoDao.gravarHistorico(cartao.id, req.body.dispositivo, aut_num);
+                                return resp.json({autorizado:aut_bool});
+                            }).catch(erro => {
+                                resp.status(500).end();
+                                console.log(erro);
+                            });;
+                    }
+                })
+                .catch(erro => {
+                    resp.status(500).end();
+                    console.log(erro);
+                });
         };
     }
 
--- backend/src/app/infra/historico-dao.js
+++ backend/src/app/infra/historico-dao.js
@@ -18,7 +18,99 @@ class HistoricoDao {
             )
         });
     }
+    buscaPorId(id) {
 
+        return new Promise((resolve, reject) => {
+            this._db.get(
+                `
+                    SELECT *
+                    FROM historico
+                    WHERE id = ?
+                `,
+                [id],
+                (erro, historico) => {
+                    if (erro) {
+                        return reject('Não foi possível encontrar o historico!');
+                    }
+                    return resolve(historico);
+                }
+            );
+        });
+    }
+
+    gravarHistorico(cartaoId, dispositivo, autorizado) {
+        return new Promise((resolve, reject) => {
+            this._db.run(`
+                INSERT INTO historico (
+                    cartao, 
+                    dispositivo,
+                    autorizado
+                ) values (?,?,?)
+                `,
+                [
+                    cartaoId,
+                    dispositivo,
+                    autorizado
+                ],
+                function (err) {
+                    if (err) {
+                        console.log(err);
+                        return reject('Não foi possível gravar o histórico!');
+                    }
+
+                    resolve();
+                }
+            )
+        });
+    }
+    verificarAutorizado(rfid, dispositivo){
+        return new Promise((resolve, reject) => {
+            this._db.get(
+                `
+                    SELECT cartao_dispositivo.autorizado
+                    FROM cartao_dispositivo
+                    INNER JOIN cartao on cartao_dispositivo.cartao = cartao.id
+                    WHERE cartao_dispositivo.dispositivo = ? and cartao.rfid = ?
+                `,
+                [
+                    dispositivo,
+                    rfid
+                ],
+                (erro, historico) => {
+                    if (erro) {
+                        return reject('Não foi possível encontrar o historico!');
+                    }
+                    return resolve(historico);
+                }
+            );
+        });
+    }
+
+    adicionarCartao(cartao, dispositivo, autorizado) {
+        return new Promise((resolve, reject) => {
+            this._db.run(`
+                INSERT INTO cartao_dispositivo (
+                    dispositivo, 
+                    cartao,
+                    autorizado
+                ) values (?,?,?)
+                `,
+                [
+                    dispositivo,
+                    cartao,
+                    autorizado
+                ],
+                function (err) {
+                    if (err) {
+                        console.log(err);
+                        return reject('Não foi possível adicionar o cartão a este dispositivo!');
+                    }
+
+                    resolve();
+                }
+            )
+        });
+    }
 }
 
 module.exports = HistoricoDao;
