64a00bfde2fd653b9f8c263a072e07fb1896f34f
leticiapmay99 <43357500+leticiapmay99@users.noreply.github.com>
2020-11-30 20:08:08 -0300

    novo usuario

--- frontend/src/pages/user/User.jsx
+++ frontend/src/pages/user/User.jsx
@@ -4,25 +4,25 @@ import { ErrorMessage, Formik, Form, Field } from 'formik';
 import { Image } from 'react-bootstrap';
 import * as yup from 'yup';
 import axios from 'axios';
-import { history } from '../../history';
 
 import LoginImage from '../../assets/login-header.svg';
 
 const User = () => {
 
-    const handleSubmit = values => {
-        axios.post('http://localhost:8080/api/usuario/login', values)
+    const handleCreate = values => {
+        axios.post('http://localhost:8080/api/usuario/adicionar', values)
         .then(resp => {
             const { data } = resp
+            console.log(resp)
             if(data) {
                 localStorage.setItem('app-token', data)
-                history.push('/')
             }
         })
     }
     const validations = yup.object().shape({
+        nome: yup.string().required(),
         email: yup.string().email().required(),
-        password: yup.string().min(5).required()
+        senha: yup.string().min(5).required()
     })
 
     return (
@@ -31,23 +31,30 @@ const User = () => {
                 <Row className="justify-content-center">
                 <Col lg={6} className="login-container">
                         <Image src={LoginImage} fluid/>
-                        <Formik initialValues={{}} onSubmit={handleSubmit} validationSchema={validations}>
+                        <Formik initialValues={{}} onSubmit={handleCreate} validationSchema={validations}>
                             <Form className="login-form">
                                 <Label className="label-login">Login</Label>
+                                
                                 <ErrorMessage component="span" name="email" className="form-error"></ErrorMessage>
                                 <div className="form-group">
-                                
+                                    <Label className="p-text-title" >E-mail</Label>
                                     <Field name="email" type="text" className="form-field"/>
                                 </div>
-                                    
-                                <ErrorMessage component="span" name="password" className="form-error"></ErrorMessage>
+
+                                <ErrorMessage component="span" name="nome" className="form-error"></ErrorMessage>
                                 <div className="form-group">
+                                    <Label className="p-text-title" >Nome</Label>
+                                    <Field name="nome" type="text" className="form-field"/>
+                                </div>
                                     
-                                    <Field name="password" type="password" className="form-field"/>
+                                <ErrorMessage component="span" name="senha" className="form-error"></ErrorMessage>
+                                <div className="form-group">
+                                    <Label className="p-text-title" >senha</Label>
+                                    <Field name="senha" type="password" className="form-field"/>
                                 </div>
                                    
                                 <div className="form-group">
-                                    <button className="login-button" type="submit">Entrar</button>
+                                    <button className="login-button" type="submit">Cadastrar</button>
                                 </div>
                             </Form>
                         </Formik>
